a154539e3069970f0b69a9e4e5e8c34b98de31a7
AminovJamshid <aminovjamshid777@gmail.com>
2024-08-09 12:02:24 +0500

    MINOR: added Ads gateway

--- index.php
+++ index.php
@@ -4,11 +4,21 @@ require_once "bootstrap.php";
 use App\User;
 $user = new User();
 
-$user->create(
-    username: "Werther",
-    position: "capitan",
-    gender: "male",
-    phone: "998997777777"
-);
+//$user->create(
+//    username: "Werther",
+//    position: "capitan",
+//    gender: "male",
+//    phone: "998997777777"
+//);
+
+$ads = new App\Ads();
+$ads->createAds('title',
+    'description', 2, 2, 2,
+    'address', 100, 2);
+//$ads -> updateAds(3,'TITLE',
+//    'DESCRIPTION',2,2,2,
+//    "Chilonzor",100,2);
+//$ads -> deleteAds(3);
+
 
 var_dump($user);
