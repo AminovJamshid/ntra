cfadff7e7304ca3f3ed41bd8ca0acd4ef7e16a96
AminovJamshid <aminovjamshid777@gmail.com>
2024-09-12 13:32:43 +0500

    MINOR: add dumb.sql

---
+++ dump.sql
@@ -0,0 +1,227 @@
+-- MySQL dump 10.13  Distrib 8.3.0, for macos14.2 (arm64)
+--
+-- Host: localhost    Database: ntra
+-- ------------------------------------------------------
+-- Server version	8.3.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ads`
+--
+
+DROP TABLE IF EXISTS `ads`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ads` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `description` text,
+  `user_id` int DEFAULT NULL,
+  `status_id` int DEFAULT NULL,
+  `branch_id` int DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  `rooms` int DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ads_status` (`status_id`),
+  KEY `ads_branch` (`branch_id`),
+  KEY `ads_user` (`user_id`),
+  CONSTRAINT `ads_branch` FOREIGN KEY (`branch_id`) REFERENCES `branch` (`id`),
+  CONSTRAINT `ads_status` FOREIGN KEY (`status_id`) REFERENCES `status` (`id`),
+  CONSTRAINT `ads_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ads`
+--
+
+LOCK TABLES `ads` WRITE;
+/*!40000 ALTER TABLE `ads` DISABLE KEYS */;
+INSERT INTO `ads` VALUES (2,'10 xonali kvartira isjaraga beriladi','desc',6,1,1,'Chilonzor',100,4,'2024-08-09 13:09:43',NULL),(3,'20 xonali kvartira isjaraga beriladi','desc 2',6,1,1,'Xorazm',200,4,'2024-08-09 13:09:43',NULL),(5,'Dacha arenda','Super cheap and awesome dacha',6,1,1,'Malibu beach',10000,12,'2024-08-16 14:56:28',NULL),(6,'Title','desc',6,1,1,'adress',100,1,'2024-08-20 12:14:45',NULL),(7,'Title','desc',6,1,1,'address',100,100,'2024-08-20 12:16:32',NULL),(8,'Title','desc',6,1,1,'address',100,100,'2024-08-20 12:17:48',NULL),(9,'Title','desc',6,1,1,'address',100,100,'2024-08-20 12:18:57',NULL),(10,'','',6,1,1,'',0,0,'2024-08-20 12:23:22',NULL),(11,'','',6,1,1,'',0,0,'2024-08-20 12:37:43',NULL),(12,'','',6,1,1,'',0,0,'2024-08-20 12:37:59',NULL),(13,'123','123',6,1,1,'213',123,123,'2024-08-20 14:32:37',NULL),(14,'098','098',6,1,1,'098',987,98,'2024-08-20 14:35:01',NULL),(15,'Title','description',6,1,1,'Address',100,12,'2024-08-22 13:14:14',NULL),(16,'Title','description',6,1,1,'Address',100,12,'2024-08-22 13:15:03',NULL),(17,'Title','description',6,1,1,'Address',100,12,'2024-08-22 13:17:14',NULL),(18,'Tit','13',6,1,1,'123',213,123,'2024-08-22 13:21:17',NULL),(19,'Tit','13',6,1,1,'123',213,123,'2024-08-22 13:46:43',NULL),(20,'Tit','13',6,1,1,'123',213,123,'2024-08-22 13:47:06',NULL),(21,'Tit','13',6,1,1,'123',213,123,'2024-08-22 13:48:08',NULL),(22,'Tit','13',6,1,1,'123',213,123,'2024-08-22 13:48:19',NULL),(23,'Tit','13',6,1,1,'123',213,123,'2024-08-22 13:48:41',NULL),(24,'Tit','13',6,1,1,'123',213,123,'2024-08-22 13:49:12',NULL),(25,'1','12',5,1,2,'й3',123,23,'2024-08-22 13:59:59',NULL),(30,'New ad branch 1','new desc                                                            ',5,1,1,'new address',100,1,'2024-09-08 19:13:38',NULL),(31,'New ad branch 20 xonali kvartira isjaraga beriladi\n','new desc                                                            ',5,1,2,'new address',100,1,'2024-09-08 19:27:17',NULL),(32,'New ad2','new desc22',5,1,2,'new address',200,222,'2024-09-08 19:27:40','2024-09-08 20:10:41'),(33,'New ad2321','new desc',5,1,2,'new address',200,232,'2024-09-08 19:29:57','2024-09-08 20:21:17');
+/*!40000 ALTER TABLE `ads` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ads_image`
+--
+
+DROP TABLE IF EXISTS `ads_image`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ads_image` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ads_id` int DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ads_image_ads_id_fk` (`ads_id`),
+  CONSTRAINT `ads_image_ads_id_fk` FOREIGN KEY (`ads_id`) REFERENCES `ads` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ads_image`
+--
+
+LOCK TABLES `ads_image` WRITE;
+/*!40000 ALTER TABLE `ads_image` DISABLE KEYS */;
+INSERT INTO `ads_image` VALUES (1,14,'66c4a995eef40___image 1.png'),(2,17,'66c73a5ae6ba3___image 4.png'),(7,30,'66ddf7624984f___image 5.png'),(8,31,'66ddfa956f4e8___image 5.png'),(9,32,'66ddfaac40d32___image 5.png'),(10,33,'66ddfb3516bbb___image 5.png');
+/*!40000 ALTER TABLE `ads_image` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `branch`
+--
+
+DROP TABLE IF EXISTS `branch`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `branch` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `branch`
+--
+
+LOCK TABLES `branch` WRITE;
+/*!40000 ALTER TABLE `branch` DISABLE KEYS */;
+INSERT INTO `branch` VALUES (1,'Farg\'ona','Farg\'ona, Farg\'ona, 1','2024-08-09 13:08:20',NULL),(2,'Xorazm','Xorazm, Xorazm, 2','2024-08-01 10:35:48',NULL);
+/*!40000 ALTER TABLE `branch` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'admin'),(2,'user');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `status`
+--
+
+DROP TABLE IF EXISTS `status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `status` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `status`
+--
+
+LOCK TABLES `status` WRITE;
+/*!40000 ALTER TABLE `status` DISABLE KEYS */;
+INSERT INTO `status` VALUES (1,'Active'),(2,'Paused');
+/*!40000 ALTER TABLE `status` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_roles`
+--
+
+DROP TABLE IF EXISTS `user_roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_roles` (
+  `user_id` int DEFAULT NULL,
+  `role_id` int DEFAULT NULL,
+  KEY `user_roles_roles_id_fk` (`role_id`),
+  KEY `user_roles_users_id_fk` (`user_id`),
+  CONSTRAINT `user_roles_roles_id_fk` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `user_roles_users_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_roles`
+--
+
+LOCK TABLES `user_roles` WRITE;
+/*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
+INSERT INTO `user_roles` VALUES (5,1),(6,1);
+/*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `position` varchar(255) DEFAULT NULL,
+  `gender` enum('male','female') DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_pk` (`phone`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (5,'user@mail.com','123','Bekzodbro','ustoz','male','7777777777','2024-08-08 14:19:10','2024-08-08 14:26:36'),(6,NULL,'123','Nasriddinbro','pahan','male','998997777778','2024-08-08 14:40:39',NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-09-11 18:50:24
